429cbd9032e33606b0900b3e3fffe2d76705fdfd
Taha <mohammedtahahalim@proton.me>
2025-09-28 23:35:11 +0100

    finished the main logic for authSlice and Auth routing wrapper

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import testSlice from "./slices/testSlice";
+import authSlice from "../features/auth/authSlice";
 
 export const NisekoStore = configureStore({
   reducer: {
     test: testSlice,
+    auth: authSlice,
   },
 });
 
-export type AppDisptach = typeof NisekoStore.dispatch;
+export type AppDispatch = typeof NisekoStore.dispatch;
 export type RootState = ReturnType<typeof NisekoStore.getState>;
--- src/features/auth/Auth.tsx
+++ src/features/auth/Auth.tsx
@@ -1,3 +1,21 @@
+import { useDispatch, useSelector } from "react-redux";
+import type { AppDispatch, RootState } from "../../app/store";
+import { useEffect } from "react";
+import { checkAuthentication } from "./authSlice";
+import { Navigate, Outlet } from "react-router-dom";
+import Loader from "../../components/Loader";
+
 export default function Auth() {
-  return <div>Auth</div>;
+  const { loading, error, isAuthenticated, redirectTo } = useSelector(
+    (state: RootState) => state.auth
+  );
+  const dispatch = useDispatch<AppDispatch>();
+  useEffect(() => {
+    dispatch(checkAuthentication());
+  }, []);
+  if (loading) return <Loader />;
+  if (redirectTo) return <Navigate to={redirectTo} />;
+  if (error) return <div>{error}</div>;
+  if (!isAuthenticated) return null;
+  return <Outlet />;
 }
