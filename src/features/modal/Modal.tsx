a66af04d535f83084ff92ce54be4c73ebf1e7c03
Taha <mohammedtahahalim@proton.me>
2025-09-30 20:58:45 +0100

    finished the mail useModal hook logic

--- src/features/modal/Modal.tsx
+++ src/features/modal/Modal.tsx
@@ -1,3 +1,42 @@
-export default function Modal() {
-  return <div>Modal</div>;
+import { Box, styled } from "@mui/material";
+
+interface ModalProps {
+  fullScreen?: boolean;
+  blurBackground?: boolean;
+  sx: React.CSSProperties;
+  children: React.ReactNode;
+}
+
+const ModalContainer = styled(Box)<{
+  fullScreen: boolean;
+  blurBackground: boolean;
+  sx: React.CSSProperties;
+}>(({ theme, fullScreen, blurBackground, sx }) => ({
+  width: fullScreen ? "100vw" : "",
+  height: fullScreen ? "100vh" : "",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  backgroundColor: blurBackground
+    ? "rgba(255, 255, 255, 0.3)"
+    : theme.palette.headfoot?.main,
+  ...sx,
+}));
+
+const ModalWrapper = styled(Box)({});
+
+export default function Modal({
+  fullScreen = false,
+  blurBackground = true,
+  sx = {},
+}: ModalProps) {
+  return (
+    <ModalContainer
+      fullScreen={fullScreen}
+      blurBackground={blurBackground}
+      sx={sx}
+    >
+      <ModalWrapper>Modal</ModalWrapper>
+    </ModalContainer>
+  );
 }
--- src/features/modal/useModal.ts
+++ src/features/modal/useModal.ts
@@ -1,1 +1,71 @@
-export default function useModal() {}
+import { useEffect, useRef, useState, type RefObject } from "react";
+
+interface UseModalRefReturns<TParent, TModal> {
+  parentRef: RefObject<TParent | null>;
+  modalRef: RefObject<TModal | null>;
+  isOpen: boolean;
+  openModal: () => void;
+  closeModal: () => void;
+}
+
+export default function useModal<
+  TParent extends HTMLElement,
+  TModal extends HTMLElement
+>(): UseModalRefReturns<TParent, TModal> {
+  const parentRef = useRef<TParent | null>(null);
+  const modalRef = useRef<TModal | null>(null);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const lastActive = useRef<HTMLElement | null>(null);
+
+  const openModal = () => {
+    setIsOpen(true);
+    lastActive.current = document.activeElement as HTMLElement | null;
+    modalRef.current?.focus();
+  };
+  const closeModal = () => {
+    setIsOpen(false);
+    try {
+      lastActive.current?.focus();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    const detectClickOutsideModal = (e: MouseEvent) => {
+      if (
+        parentRef.current &&
+        modalRef.current &&
+        !parentRef.current.contains(e.target as Node) &&
+        !modalRef.current.contains(e.target as Node)
+      ) {
+        closeModal();
+      }
+    };
+    window.addEventListener("click", detectClickOutsideModal);
+    return () => {
+      window.removeEventListener("click", detectClickOutsideModal);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!isOpen) return;
+    const stopOtherEvents = (e: Event) => e.stopPropagation();
+    window.addEventListener("scroll", stopOtherEvents, { passive: false });
+    window.addEventListener("touchmove", stopOtherEvents, { passive: false });
+    return () => {
+      window.removeEventListener("scroll", stopOtherEvents);
+      window.removeEventListener("touchmove", stopOtherEvents);
+    };
+  }, [isOpen]);
+
+  useEffect(() => {
+    const escapeClick = (e: KeyboardEvent) => {
+      if (e.key === "Escape" && isOpen) closeModal();
+    };
+    window.addEventListener("keydown", escapeClick);
+    return () => window.removeEventListener("keydown", escapeClick);
+  }, [isOpen]);
+
+  return { parentRef, modalRef, isOpen, openModal, closeModal };
+}
