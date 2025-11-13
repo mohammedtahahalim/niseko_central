3d7bb54bb57cfd374d4fd994e1894cb41eeff1b7
Taha Halim <mohammedtahahalim@proton.me>
2025-11-13 17:47:34 +0100

    finished the main core of faq page

---
+++ src/components/LinkTitle.tsx
@@ -0,0 +1,74 @@
+import { Box, styled, Typography } from "@mui/material";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+
+const LinkTitleWrapper = styled(Box)(({ theme }) => ({
+  width: "fit-content",
+  padding: "10px",
+  color: theme.palette.icons?.main,
+  display: "flex",
+  alignItems: "center",
+  gap: "5px",
+}));
+
+const LinkTitleContent = styled(Typography)(({ theme }) => ({
+  fontSize: "0.7rem",
+  cursor: "pointer",
+  color: theme.palette.icons?.main,
+  textTransform: "capitalize",
+  fontFamily: "Inter",
+  fontWeight: "400",
+  "&:hover": {
+    color: theme.palette.primary.main,
+  },
+}));
+
+const Icon = styled(Box)(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  color: theme.palette.icons?.main,
+}));
+
+export default function LinkTitle() {
+  const pathname = useLocation().pathname.split("/")[1];
+  const { t, i18n } = useTranslation();
+  const navigate = useNavigate();
+
+  const handleKeyboardClick = (e: React.KeyboardEvent, link: string) => {
+    if (e.key === "Enter") {
+      navigate(link);
+    }
+  };
+
+  return (
+    <LinkTitleWrapper>
+      <LinkTitleContent
+        variant="subtitle2"
+        onClick={() => navigate("/")}
+        onKeyDown={(e) => handleKeyboardClick(e, "/")}
+        tabIndex={0}
+        role="button"
+      >
+        {t("accomodation.link_title.home")}
+      </LinkTitleContent>
+      <Icon>
+        {i18n.language === "ar" ? (
+          <ArrowBackIosIcon sx={{ fontSize: "0.6rem" }} color="inherit" />
+        ) : (
+          <ArrowForwardIosIcon sx={{ fontSize: "0.6rem" }} color="inherit" />
+        )}
+      </Icon>
+      <LinkTitleContent
+        variant="subtitle2"
+        onClick={() => navigate(`/${pathname}`)}
+        onKeyDown={(e) => handleKeyboardClick(e, `/${pathname}`)}
+        tabIndex={0}
+        role="button"
+      >
+        {pathname.toLocaleUpperCase()}
+      </LinkTitleContent>
+    </LinkTitleWrapper>
+  );
+}
