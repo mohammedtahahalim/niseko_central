defe27c1ebc8b1c759927c8b79e9c89bc6958a2e
Taha Halim <mohammedtahahalim@proton.me>
2025-11-20 07:06:02 +0100

    minor tweaks

--- src/features/concierge/useArticleCount.ts
+++ src/features/concierge/useArticleCount.ts
@@ -1,1 +1,36 @@
-export default function useArticleCount() {}
+import { useEffect, useState } from "react";
+import { debouncer } from "../../utils/Constants";
+
+export default function useArticleCount() {
+  const [slideCount, setSlideCount] = useState<number>(2);
+  const [maxHeight, setMaxHeight] = useState<string>("350px");
+
+  useEffect(() => {
+    const onResize = debouncer(() => {
+      const winwidth = window.innerWidth;
+      if (winwidth > 1300) {
+        setMaxHeight("350px");
+        setSlideCount(2);
+      }
+      if (winwidth < 1300 && winwidth > 1024) {
+        setMaxHeight("");
+        setSlideCount(2);
+      }
+      if (winwidth < 1024 && winwidth > 750) {
+        setMaxHeight("350px");
+        setSlideCount(2);
+      }
+      if (winwidth < 750) {
+        setMaxHeight("350px");
+        setSlideCount(1);
+      }
+    }, 100);
+    onResize();
+    window.addEventListener("resize", onResize);
+    return () => {
+      window.removeEventListener("resize", onResize);
+    };
+  }, []);
+
+  return { slideCount, maxHeight };
+}
